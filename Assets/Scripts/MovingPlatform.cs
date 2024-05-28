0ed20f8754b7b3c24148e062b07163a9ad2ba778
DanCasber <daniela.castillo@uniat.edu.mx>
2024-05-28 17:34:47 -0600

    Mooving Platforms

---
+++ Assets/Scripts/MovingPlatform.cs
@@ -0,0 +1,63 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class MovingPlatform : MonoBehaviour
+{
+    [SerializeField] private Transform platform;
+    [SerializeField] private Transform startPoint;
+    [SerializeField] private Transform endPoint;
+    [SerializeField] private float speed = 1f;
+
+    private Vector3 targetPosition;
+
+    private void Start()
+    {
+        targetPosition = endPoint.position;
+    }
+
+    private void Update()
+    {
+        platform.position = Vector3.Lerp(platform.position, targetPosition, speed * Time.deltaTime);
+
+        // Move the platform towards the target position
+        //platform.position = Vector3.MoveTowards(platform.position, targetPosition, speed * Time.deltaTime);
+
+        // Check if the platform has reached the target position
+        if (Vector3.Distance(platform.position, targetPosition) < 0.1f)
+        {
+            // Swap the target position
+            targetPosition = targetPosition == endPoint.position ? startPoint.position : endPoint.position;
+        }
+    }
+
+    void OnDrawGizmosSelected()
+    {
+        // Draw a line in the editor to visualize the movement path
+        // Only draw when the game is not running
+        if (!Application.isPlaying)
+        {
+            Gizmos.DrawLine(platform.transform.position, startPoint.transform.position);
+            Gizmos.DrawLine(platform.transform.position, endPoint.transform.position);
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "Player")
+        {
+            Debug.Log("Trigger Enter Player");
+            other.transform.parent = this.transform;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            Debug.Log("Trigger Exit Player");
+            other.transform.parent = null;
+        }
+    }
+}
