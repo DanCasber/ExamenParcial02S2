ccc6a7ff651cf8c86ea1f82cf45fee3c49e5f7fb
DanCasber <daniela.castillo@uniat.edu.mx>
2024-05-31 18:14:38 -0600

    Cursor lock on pause menu fix

--- Assets/Scripts/PauseMenu.cs
+++ Assets/Scripts/PauseMenu.cs
@@ -26,6 +26,8 @@ public class PauseMenu : MonoBehaviour
 
     void PauseGame()
     {
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
         pauseMenu.SetActive(true);
         Time.timeScale = 0f;
         isPaused = true;
@@ -33,6 +35,8 @@ public class PauseMenu : MonoBehaviour
 
     public void ResumeGame()
     {
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
         pauseMenu.SetActive(false);
         Time.timeScale = 1f;
         isPaused = false;
